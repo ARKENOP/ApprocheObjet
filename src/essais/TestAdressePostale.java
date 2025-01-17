c50bb451c619a5a98430ab47f9c21e25652e6d02
Paul SUPIOT <psupiot44@gmail.com>
2025-01-17 16:24:57 +0100

    change TP file

--- src/essais/TestAdressePostale.java
+++ src/essais/TestAdressePostale.java
@@ -6,7 +6,8 @@ public class TestAdressePostale {
     public static void main(String[] args) {
         AdressePostale adresse1 = new AdressePostale(1, "rue de la Paix", 75000, "Paris");
         AdressePostale adresse2 = new AdressePostale(2, "rue de la Liberté", 69000, "Lyon");
-        adresse1.afficheAdresse();
-        adresse2.afficheAdresse();
+
+        System.out.println(adresse1);
+        System.out.println(adresse2);
     }
 }
