a83d4ffb7207048a14aa38e25b98bc6ede90bbf0
Paul SUPIOT <psupiot44@gmail.com>
2025-01-17 16:21:40 +0100

    TP Banque

---
+++ src/fr/epsi/banque/entites/CompteTaux.java
@@ -0,0 +1,16 @@
+package fr.epsi.banque.entites;
+
+public class CompteTaux extends Compte{
+
+    private double taux;
+
+    public CompteTaux(int numero, double solde, double taux) {
+        super(numero, solde);
+        this.taux = taux;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Taux : " + taux;
+    }
+}
